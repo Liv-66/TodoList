dd96c3561b6de90b3a1d429c5440b67cca79927b
Liv Huang <livhuang95@gmail.com>
2020-11-08 09:02:43 +0800

    feat: add user login feature via passport

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const User = require('../../models/user');
@@ -6,12 +7,19 @@ const User = require('../../models/user');
 router.get('/login', (req, res) => {
   res.render('login');
 });
-router.post('/login', (req, res) => {});
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })
+);
 router.get('/register', (req, res) => {
   res.render('register');
 });
 router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;  User.findOne({ email }).then((user) => {
+  const { name, email, password, confirmPassword } = req.body;
+  User.findOne({ email }).then((user) => {
     if (user) {
       console.log('User already exists.');
       res.render('register', {
