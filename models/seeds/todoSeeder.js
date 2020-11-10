636aa94a2b226533e66c313f9870c12f9c08d6da
Liv Huang <livhuang95@gmail.com>
2020-11-10 12:52:46 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,36 @@
-const Todo = require('../todo')
-const db = require('../../config/mongoose')
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const Todo = require('../todo');
+const User = require('../user');
+const bcrypt = require('bcryptjs');
+const db = require('../../config/mongoose');
+
+const SEED_USER = {
+  name: 'Loulou',
+  email: 'lou@example.com',
+  password: 'aaa',
+};
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
-
-  console.log('done')
-})
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Todo.create({ name: `name-${i}`, userId })
+        )
+      );
+    })
+    .then(() => {
+      console.log('done');
+      process.exit();
+    });
+});
